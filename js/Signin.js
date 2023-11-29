83b39cfdacfd9c9632ad1a2b8f12504694516dfb
isaac <ivaughn@usca.edu>
2023-11-29 12:24:53 -0500

    testing

--- js/Signin.js
+++ js/Signin.js
@@ -75,17 +75,15 @@ $('#Login').submit(function (e) {
 // })
 
 import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
-
+$('#googleLog').click(function(){
 const auth = getAuth();
 signInWithPopup(auth, provider)
   .then((result) => {
     // This gives you a Google Access Token. You can use it to access the Google API.
     const credential = GoogleAuthProvider.credentialFromResult(result);
     const token = credential.accessToken;
-    // The signed-in user info.
     const user = result.user;
-    // IdP data available using getAdditionalUserInfo(result)
-    // ...
+  
   }).catch((error) => {
     // Handle Errors here.
     const errorCode = error.code;
@@ -95,4 +93,6 @@ signInWithPopup(auth, provider)
     // The AuthCredential type that was used.
     const credential = GoogleAuthProvider.credentialFromError(error);
     // ...
+    console.log(errorCode+", "+errorMessage+", "+email+", "+credential)
   });
+};
